2cb0546b040b1ebd17fb5e5a87ed139524820eb3
javi-lopez02 <mjlphone0210@gmail.com>
2025-01-09 23:41:38 -0500

    Fix Error Tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,12 @@ import { nextui } from "@nextui-org/react";
       primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
     } */
 
-module.exports = {
+export default {
   content: [
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
-    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
