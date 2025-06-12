0cf092a2c4f49ad91a3f125453d83da06d0e18ad
javi-lopez02 <mjlphone0210@gmail.com>
2025-06-12 13:24:12 -0400

    update

--- src/conf.ts
+++ src/conf.ts
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:4000/api"
+export const API_URL = "https://hostaly-e-backend.onrender.com/api"
 
 export const MESSAGE_URL=`https://wa.me/${5363939196}?text=`
